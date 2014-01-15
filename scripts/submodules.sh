862e370db47d7adfc754c286f3e0c95d46a5030b
Badhri Sampathkumar <sampath@badhri.net>
2014-01-14 22:53:31 -0800

    update scripts and gitignore

--- scripts/submodules.sh
+++ scripts/submodules.sh
@@ -1,6 +1,19 @@
+#!/bin/bash
 SCRIPT_ROOT=`dirname "$0"`
-SCRIPT_ROOT=`( cd "$SRC_ROOT" && pwd )`
+SCRIPT_ROOT=`( cd "$SCRIPT_ROOT" && pwd )`
 SRC_ROOT=${SCRIPT_ROOT}/..
 GHOST_DIR=${SRC_ROOT}/blog/ghost
+THEME=ghostium
+GHOSTIUM_DIR=${SRC_ROOT}/blog/${THEME}
 CUR_GHOST_VER=0.3.3
-cd ${GHOST_DIR} && git checkout ${CUR_GHOST_VER}
+
+### Get the stable ghost version
+echo "cd ${GHOST_DIR} && git checkout ${CUR_GHOST_VER}"
+
+### copy ghostium from the submodule dir, to the 
+### themes dir
+echo cp -pr ${GHOSTIUM_DIR} ${GHOST_DIR}/content/themes
+
+function clean() {
+   echo rm -fr ${GHOST_DIR}/content/themes/${THEME}
+}
